3ed8a09992b4630b29510dbe5082260f2bfe0460
William Fry <wrfry95@gmail.com>
2018-02-27 11:50:52 -0500

    Add dependency to dialogues

--- app/slack_integration/migrations/0006_auto_20180227_1511.py
+++ app/slack_integration/migrations/0006_auto_20180227_1511.py
@@ -6,6 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('dialogues', '0003_remove_reply_origin_slack_event'),
         ('slack_integration', '0005_auto_20180222_2128'),
     ]
 

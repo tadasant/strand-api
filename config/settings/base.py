2a777a9f3246afc4b0952d67bc984fe369c04061
William Fry <wrfry95@gmail.com>
2018-02-28 13:16:41 -0500

    Move to v0.1.1

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,7 +24,7 @@ else:
     from config.settings.local import *
 
 # Release version
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

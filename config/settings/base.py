d780b0bc3ef8a033a54e8b170c967fc7d852eb68
William Fry <wrfry95@gmail.com>
2018-03-07 13:35:55 -0500

    Increment version number

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,7 +24,7 @@ else:
     from config.settings.local import *
 
 # Release version
-VERSION = '0.1.2'
+VERSION = '0.2.0'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

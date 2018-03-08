f54435013d458bc7cd449c6277b5526a1f8b614b
William Fry <wrfry95@gmail.com>
2018-03-08 10:46:15 -0500

    Remove unnecessary packages and settings

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -40,7 +40,6 @@ AWS_LOCATION = 'static'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
